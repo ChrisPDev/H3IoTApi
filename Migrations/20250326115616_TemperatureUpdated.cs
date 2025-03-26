eb263dd0d32df8ca6d4b8f6e7db71a6803ee9297
Christoffer Møller Petersen <chrisp.dev@outlook.com>
2025-03-26 12:58:09 +0100

    new migration and updated database

---
+++ Migrations/20250326115616_TemperatureUpdated.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace H3IoTApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TemperatureUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "temperatureReading");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "temperatureReading");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "temperatureReading",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "temperatureReading",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
